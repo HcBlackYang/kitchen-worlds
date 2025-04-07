dfe01cee1138a1068086b31228e1e7ddb62a32e0
HcBlackYang <1607834283@qq.com>
2025-04-07 19:18:30 +0800

    添加环境与训练脚本

--- examples/test_parse_lisdf.py
+++ examples/test_parse_lisdf.py
@@ -2,7 +2,8 @@ from config import ASSET_PATH
 from os.path import join, dirname
 from lisdf.parsing.sdf_j import load_sdf
 
-test_cases = ['kitchen_counter', 'm0m_0_test', 'm0m_joint_test'] 
+# test_cases = ['kitchen_counter', 'm0m_0_test', 'm0m_joint_test']
+test_cases = ['kitchen_basics']
 scene_paths = [join(ASSET_PATH, 'scenes', f'{l}.lisdf') for l in test_cases]
 # scene_paths = ['/home/yang/Documents/kitchen-worlds/outputs/test_full_kitchen/1230-140406_original_3/scene.lisdf']
 
@@ -12,3 +13,20 @@ if __name__ == "__main__":
         models = lissdf_results.worlds[0].models
         print(f"{lisdf_path} has {len(models)} models\n", end='\r')
     print(f'finished parsing {len(test_cases)} scene files')
+
+    for model in models:
+        print(f"Available attributes in Model: {dir(model)}")
+
+    for model in models:
+        print(f"Converting {model.name} to URDF...")
+        try:
+            urdf_string = model.to_urdf()
+            with open(f"{model.name}.urdf", "w") as f:
+                f.write(urdf_string)
+            print(f"Saved {model.name}.urdf")
+        except FileNotFoundError as e:
+            print(f"Skipping {model.name}, missing file: {e}")
+
+
+
+
